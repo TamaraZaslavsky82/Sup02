503330fe5eb3bf93e3ff147d45d3ca6289771e38
MajoVera <nandadevi97816@gmail.com>
2023-06-28 19:06:01 -0400

    se anadio el nav

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Description from './Description'
+import Nav from '../Nav/Nav.jsx'
 
 export default function Home() {
   return (
     <div>
+      <Nav />
       <Description/>
-      
+     
     </div>
   )
 }
